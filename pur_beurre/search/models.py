0effbb08b121face5ba1da029a6eb551865ff8ff
Thomas Duval <tomas1905@hotmail.fr>
2021-09-22 10:29:55 +0200

    MAJ Views ( Search specs aliment or substitute )

--- pur_beurre/search/models.py
+++ pur_beurre/search/models.py
@@ -3,10 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Aliment(models.Model):
+    
     pnns_groups_1 = models.CharField(max_length=100)
     product_name = models.CharField(max_length=100)
     nutriscore = models.CharField(max_length=1)
-    url = models.URLField()
+    url = models.URLField(unique=True)                               # Unique=True to be sure to not introduce a product more than 1 time
 
 class User(models.Model):
     user_name = models.CharField(max_length=100)
--- pur_beurre/search/views.py
+++ pur_beurre/search/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader 
+from django.template import loader
+
+from search.models import Aliment
 
 def index(request):
     template = loader.get_template('search/index.html')
@@ -17,3 +19,44 @@ def aliment(request):
 def compte(request):
     template = loader.get_template('search/compte.html')
     return HttpResponse(template.render(request=request))
+
+def info_aliment(request):
+
+    obj = str(request.GET)                                                       # On utilise la méthode GET sur l'objet request de classe WSGIRequest. Cette méthode de la classe WSGIRequest permet de récupérer un dictionnaire qui contient les arguments passés à l'URL
+    query = request.GET['query']                                                 # On récupère la requête dans le dictionnaire ainsi créé. Ici, on attends de la requête qu'elle soit l'URL d'un produit contenu dans la table search_aliment de la DB pur_beurre            
+
+    result = list(Aliment.objects.filter(url=query).values())                    # Ici, on va récupérer l'instance de la base de donnée dont l'URL correspond à celle passée en paramètre. Par défaut, c'est un objet Django QuerySet qui est retourné. On va donc demander à ce que les attributs de cet objet soient retournées ( methode .values() ), puis on demande ensuite à ce que l'objet QuerySet soit converti en liste ( list(QuerySet_obj) )
+
+    nutriscore = result[0]["nutriscore"]                                         # On attribue les données de cette liste qui nous intéressent à des variables
+    category = result[0]["pnns_groups_1"]
+    link_OFF = result[0]["url"]
+
+    message = ["propriété GET : {} et requête : {}".format(obj, query)," ", nutriscore, " ", category, " ", link_OFF]
+
+    return HttpResponse(message)                                                 # ... Et on s'en sert dans les templates !
+
+def search_substitute(request):
+
+    obj = str(request.GET)
+    query = request.GET['query']
+
+    prod_to_replace = list(Aliment.objects.filter(product_name=query).values())  # Comme dans la vue info_aliment, on a ciblé un produit avec des des filtres dans la base de données
+    category = prod_to_replace[0]["pnns_groups_1"]
+    nutriscore = prod_to_replace[0]["nutriscore"]
+
+    substitutes = list(Aliment.objects.filter(pnns_groups_1=category).values())  # On réduit la liste des substitus possibles aux produits de la même catégorie que celui que l'utilisateur cherche à remplacer
+
+    final_sort = []                                                              # On initialise une liste vide qui va contenir les produits qui sont substituables. On va remplir cette liste dans la boucle qui vient juste en dessous
+
+    for product in substitutes:
+        if nutriscore != "a":
+            if product["nutriscore"] < nutriscore:
+                final_sort.append(product)
+
+        else:
+            if product["nutriscore"] == "a":
+                final_sort.append(product)
+
+    message = final_sort
+
+    return HttpResponse(message)                                                 # ... Et on s'en sert dans les templates !
