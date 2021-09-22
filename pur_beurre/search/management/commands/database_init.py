5b6e2cd1d2adf2a9b134e68016939673496f9e50
Thomas Duval <tomas1905@hotmail.fr>
2021-09-22 17:47:00 +0200

    Modifs Models + Update database_init.py ( broken )

--- pur_beurre/search/management/commands/database_init.py
+++ pur_beurre/search/management/commands/database_init.py
@@ -6,6 +6,7 @@ from django.core.management.base import BaseCommand                             
 import http_request
 
 from search.models import Aliment
+from search.models import Category
 
 
 class Command(BaseCommand):                                                          # On définit une nouvelle classe qui hérite de BaseCommand
@@ -19,15 +20,34 @@ class Command(BaseCommand):                                                     
                     result = http_request.Requests.get_data_from_api(line)           # On réalise nos appels à l'API OFF avec le module http_request
                     
                     for product in result["products"]:                               # Récupération des données produits dans les JSON
+                        
                         try:
                             url = product["url"]
                             category = product["pnns_groups_1"]
                             name = product["product_name"]
                             nutriscore = product["nutriscore_grade"]
+                            
+                            try:
+                                new_row_category = Category(category=category)       # On commence par insérer la categorie du produit dans la table category, qui a une contrainte d'unicité sur son champ category, puis on sauvegarde
+                                print("Ceci est une nouvelle catégorie : ", new_row_category)
+                                new_row_category.save()
+                            except:
+                                pass
+                            
+                            product_category = Category.objects.get(category=category)                 # On va chercher l'objet qui correspond à la catégorie dans la table category, afin de pouvoir le lier à l'instance de la table aliment
+                            print("Ceci est mon produit de la table catégorie : ", product_category)
+                            
+                            try:
+                                print("Je suis arrivé jusque là...")
+                                new_row_aliment = Aliment(pnns_groups_1=product_category, product_name=name, nutriscore=nutriscore, url=url) # On insère les données dans la table Aliment, puis on sauvegarde les modifications après chaque insertion
+                                print("Ceci est mon instance de la table Aliment : ", new_row_aliment)
+                                new_row_aliment.save()
 
-                            new_row = Aliment(pnns_groups_1=category, product_name=name, nutriscore=nutriscore, url=url) # On insère les données dans la table Aliment, puis on sauvegarde les modifications après chaque insertion
-                            new_row.save()
+                                print("Ok")
+                            except:
+                                print("A problem occured !!")
+                        
                         except:
                             pass
-                
+
                 print("Database successfully initialized")
--- pur_beurre/search/models.py
+++ pur_beurre/search/models.py
@@ -1,15 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+
+class Category(models.Model):
+    
+    category = models.CharField(max_length=100, unique=True)              # On met évidement une contrainte d'unicité sur les catégories
+
+
 class Aliment(models.Model):
     
-    pnns_groups_1 = models.CharField(max_length=100)
+    pnns_groups_1 = models.ForeignKey(Category, on_delete=models.CASCADE) # On s'assure que la supression d'une catégorie dans la table Category entrainera également la supression des tous les aliments qui appartiennent à cette catégorie.
     product_name = models.CharField(max_length=100)
     nutriscore = models.CharField(max_length=1)
-    url = models.URLField(unique=True)                               # Unique=True to be sure to not introduce a product more than 1 time
+    url = models.URLField(unique=True)                                    # Unique=True to be sure to not introduce a product more than 1 time
 
-class User(models.Model):
-    user_name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-    aliments = models.ManyToManyField(Aliment, related_name="favorites")
+
+class Favorites(models.Model):
+    
+    class Meta:
+        unique_together = (('user_id', 'product_id'),)                    # On pose une contrainte d'unicité composite sur les deux champs spécifiés
+        
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Aliment, on_delete=models.CASCADE)
