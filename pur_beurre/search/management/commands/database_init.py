58baef7121cb454af38399377dd7c59621a9798b
Thomas Duval <tomas1905@hotmail.fr>
2021-09-23 15:01:31 +0200

    New DB structure + Fix bug add data + vVews search

--- pur_beurre/search/management/commands/database_init.py
+++ pur_beurre/search/management/commands/database_init.py
@@ -15,38 +15,33 @@ class Command(BaseCommand):                                                     
 
     def handle(self, *args, **options):                                              # La méthode handle va implémenter le code exécuté par la commande personnalisée
         with open("html_requests.txt") as instruction:                               # On récupère les URL dans html_requests.txt
-                
+
                 for line in instruction.readlines():
                     result = http_request.Requests.get_data_from_api(line)           # On réalise nos appels à l'API OFF avec le module http_request
-                    
+
                     for product in result["products"]:                               # Récupération des données produits dans les JSON
-                        
+
                         try:
                             url = product["url"]
                             category = product["pnns_groups_1"]
                             name = product["product_name"]
                             nutriscore = product["nutriscore_grade"]
-                            
+
                             try:
                                 new_row_category = Category(category=category)       # On commence par insérer la categorie du produit dans la table category, qui a une contrainte d'unicité sur son champ category, puis on sauvegarde
-                                print("Ceci est une nouvelle catégorie : ", new_row_category)
                                 new_row_category.save()
                             except:
                                 pass
-                            
-                            product_category = Category.objects.get(category=category)                 # On va chercher l'objet qui correspond à la catégorie dans la table category, afin de pouvoir le lier à l'instance de la table aliment
-                            print("Ceci est mon produit de la table catégorie : ", product_category)
-                            
-                            try:
-                                print("Je suis arrivé jusque là...")
-                                new_row_aliment = Aliment(pnns_groups_1=product_category, product_name=name, nutriscore=nutriscore, url=url) # On insère les données dans la table Aliment, puis on sauvegarde les modifications après chaque insertion
-                                print("Ceci est mon instance de la table Aliment : ", new_row_aliment)
-                                new_row_aliment.save()
 
-                                print("Ok")
+                            product_category = Category.objects.get(category=category)   # On va chercher l'objet qui correspond à la catégorie dans la table category, afin de pouvoir le lier à l'instance de la table aliment
+
+
+                            try:
+                                new_row_aliment = Aliment(pnns_groups_1=product_category, product_name=name, nutriscore=nutriscore, url=url) # On insère les données dans la table Aliment, puis on sauvegarde les modifications après chaque insertion
+                                new_row_aliment.save()
                             except:
                                 print("A problem occured !!")
-                        
+
                         except:
                             pass
 
--- pur_beurre/search/views.py
+++ pur_beurre/search/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.template import loader
 
 from search.models import Aliment
+from search.models import Category
 
 def index(request):
     template = loader.get_template('search/index.html')
@@ -28,10 +29,11 @@ def info_aliment(request):
     result = list(Aliment.objects.filter(url=query).values())                    # Ici, on va récupérer l'instance de la base de donnée dont l'URL correspond à celle passée en paramètre. Par défaut, c'est un objet Django QuerySet qui est retourné. On va donc demander à ce que les attributs de cet objet soient retournées ( methode .values() ), puis on demande ensuite à ce que l'objet QuerySet soit converti en liste ( list(QuerySet_obj) )
 
     nutriscore = result[0]["nutriscore"]                                         # On attribue les données de cette liste qui nous intéressent à des variables
-    category = result[0]["pnns_groups_1"]
+    cat_id = result[0]["pnns_groups_1_id"]
+    category = list(Category.objects.filter(id=cat_id).values())[0]["category"]  # On récupère le nom de la catégorie dans la table category en utilisant la clé étrangère de la colonne pnns_group_1
     link_OFF = result[0]["url"]
 
-    message = ["propriété GET : {} et requête : {}".format(obj, query)," ", nutriscore, " ", category, " ", link_OFF]
+    message = [nutriscore, " ", category, " ", link_OFF]
 
     return HttpResponse(message)                                                 # ... Et on s'en sert dans les templates !
 
@@ -41,7 +43,7 @@ def search_substitute(request):
     query = request.GET['query']
 
     prod_to_replace = list(Aliment.objects.filter(product_name=query).values())  # Comme dans la vue info_aliment, on a ciblé un produit avec des des filtres dans la base de données
-    category = prod_to_replace[0]["pnns_groups_1"]
+    category = prod_to_replace[0]["pnns_groups_1_id"]
     nutriscore = prod_to_replace[0]["nutriscore"]
 
     substitutes = list(Aliment.objects.filter(pnns_groups_1=category).values())  # On réduit la liste des substitus possibles aux produits de la même catégorie que celui que l'utilisateur cherche à remplacer
