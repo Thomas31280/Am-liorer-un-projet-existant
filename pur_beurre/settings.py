8483f047f9e4342a3da1348b76900ae0ddb2bcca
Thomas Duval <tomas1905@hotmail.fr>
2021-11-10 16:32:20 +0100

    Settings

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql', # On utilise l'adaptateur postgresql
         'NAME': 'pur_beurre', # Le nom de notre DB
         'USER': 'postgres', # Notre username sur PSQL ( postgres )
-        'PASSWORD': '',
+        'PASSWORD': '',     # No passaword by default
         'HOST': '',
         'PORT': '',
     }
